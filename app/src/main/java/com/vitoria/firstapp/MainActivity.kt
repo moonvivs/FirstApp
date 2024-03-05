fc24b1cc9e2bb73857505e4a0cb515175491ea6e
vitoria.alves5@portalsesisp.org.brgit config --global user.name vitoria.alves5@portalsesisp.org.br <vitoria.alves@portalsesisp.org.br>
2024-03-05 11:40:50 -0300

    criar um layout para calcular a idade. criar um layout para validar um email e telefone

--- app/src/main/java/com/vitoria/firstapp/MainActivity.kt
+++ app/src/main/java/com/vitoria/firstapp/MainActivity.kt
@@ -2,10 +2,44 @@ package com.vitoria.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vitoria.firstapp.databinding.ActivityMainBinding
+import com.vitoria.firstapp.databinding.TelaLinearBinding
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private  val binding: ActivityMainBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEnviar.setOnClickListener {
+            var email = binding.edtEmail2.editableText.toString()
+
+            if(email.contains("@") && email.contains(".com")){
+                binding.tvEmail.text = "Email: ${email}"
+            }else{
+                binding.tvEmail.text = "Email Inválido"
+            }
+
+            var telefone = binding.edtTelefone2.editableText.toString()
+            val qtndNumero = telefone.length
+
+            if (qtndNumero == 11){
+                binding.tvTelefone.text = "Telefone: ${telefone}"
+            }else{
+                binding.tvTelefone.text = "Telefone Inválido "
+            }
+        }
     }
 }
+//            var nome = binding.edtName.editableText.toString()
+//
+//            binding.tvName.text = "Nome: " + nome
+//
+//            var anoNascimento = binding.edtAnoNascimento.editableText.toString()
+//            var anoAtual = LocalDateTime.now().year
+//            var idade = anoAtual - anoNascimento.toInt()
+//
+//            binding.tvIdade.text = "Idade: ${idade}"
